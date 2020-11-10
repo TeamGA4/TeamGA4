297316c7bb8e1cd98d7f9e50bf0b03d124e49d37
GA4 <GA4@DEV1-18>
2020-11-10 16:23:01 +0900

    譜面調整

--- Project1/Project1/Header.h
+++ Project1/Project1/Header.h
@@ -36,11 +36,18 @@ private:
 	int sound; //楽曲の音声データ
 	int notes_speed; //ノーツ速度
 	int elapsed_flame; //楽曲経過時間(フレーム単位)
+
+	char judg_txt[4][9]; //判定のテキスト表示
+	int judg_txt_time[4]; //判定のテキスト表示時間
+
+	bool longpush_ctrl[4]; //キー長押し制御
 public:
 	MusicGame(char name[], int speed); //コンストラクタ
 	void Action(); //アクション
 	void Draw(); //ドロー
-	void LoadMusic(char music_name[]); //楽曲情報読み込み関数
+	void LoadMusicNotes(char music_name[]); //楽曲情報読み込み関数
+	bool PushNotesButton(Pos notes_pos, unsigned int notes_type, int line); //ノーツ入力処理
+	bool ReleaseNotesButton(Pos notes_pos, float notes_length, unsigned int notes_type, int line); //ノーツ入力処理(ロングノーツ、キー離した時用)
 };
 
 //ノーツクラス
@@ -48,7 +55,7 @@ class Notes
 {
 private:
 	Pos pos; //座標
-	unsigned int notes_type; //ノーツタイプ(1=普通ノーツ、2=ロングノーツ、3=エクストラノーツ)
+	unsigned int notes_type; //ノーツタイプ(1=通常ノーツ、2=ロングノーツ、3=エクストラノーツ)
 	unsigned int notes_speed; //ノーツ速度
 	float notes_length; //ロングノーツの長さ(フレーム数)
 	bool delete_flg; //消去フラグ
@@ -56,5 +63,9 @@ public:
 	Notes(int lane, int speed, int type, float length); //コンストラクタ
 	void Action(); //アクション
 	void Draw(); //ドロー
+	Pos GetNotesPos(); //ノーツ座標取得関数
+	int GetNotesType(); //ノーツタイプ取得関数
+	float GetNotesLength(); //ロングノーツの長さ取得関数
+	void SetDeleteFlg(bool flg); //消去フラグ設定関数
 	bool GetDeleteFlg(); //消去フラグ取得関数
 };
--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 
 //メイン
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	//windowの設定
 
@@ -17,14 +18,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	SetAlwaysRunFlag(TRUE); //バックグラウンドでも動くように設定
 	SetDoubleStartValidFlag(TRUE); //多重起動の許可
 
-	//dxライブラリ初期化
+	//dxライブラリ初期化NN
 	if (DxLib_Init() == -1) return -1; //エラーが起きたら終了
 
 	//描画先の変更
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	char music_name[] = "Battle";
-	MusicGame* music_game = new MusicGame(music_name, 18);
+	MusicGame* music_game = new MusicGame(music_name, 8);
 
 	//メインループ
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0)
